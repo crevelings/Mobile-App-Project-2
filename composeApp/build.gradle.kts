0183d4fa60d0afbb638e80b1f61a69a4e566d3c8
Sean Creveling <crevelings@moravian.edu>
2026-04-06 16:53:37 -0400

    First Commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,100 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinSerialization) // ** ADDED for Navigation **
+    alias(libs.plugins.ksp) // ** ADDED FOR ROOM **
+//    alias(libs.plugins.androidx.room) // ** ADDED FOR ROOM **
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+            freeCompilerArgs.add("-Xexpect-actual-classes")
+        }
+    }
+
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.compose.uiToolingPreview)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.room.sqlite.wrapper) // ** ADDED FOR ROOM **
+        }
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.serialization.json) // ** ADDED for Navigation **
+            implementation(libs.androidx.navigation.compose) // ** ADDED for Navigation **
+            implementation(libs.kotlinx.datetime) // ** ADDED **
+            implementation(libs.androidx.room.runtime) // **ADDED** for Room
+            implementation(libs.androidx.sqlite.bundled) // **ADDED** for Room
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+dependencies {
+    add("kspAndroid", libs.androidx.room.compiler) // **ADDED** for Room
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler) // **ADDED** for Room
+    // add("kspIosX64", libs.androidx.room.compiler) // **ADDED** for Room
+    add("kspIosArm64", libs.androidx.room.compiler) // **ADDED** for Room
+    debugImplementation(libs.compose.uiTooling)
+}
+
+android {
+    namespace = "edu.moravian.survey"
+    compileSdk =
+        libs.versions.android.compileSdk
+            .get()
+            .toInt()
+
+    defaultConfig {
+        applicationId = "edu.moravian.survey"
+        minSdk =
+            libs.versions.android.minSdk
+                .get()
+                .toInt()
+        targetSdk =
+            libs.versions.android.targetSdk
+                .get()
+                .toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
